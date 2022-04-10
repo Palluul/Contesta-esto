9959c068caa1d96f77e9869e9cbce4698655eed6
Palluul <103381580+Palluul@users.noreply.github.com>
2022-04-10 13:08:51 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Pasamela, en la escuela te pago')
+});
+
+
+
+noBtn.addEventListener('click',function () {
+    alert('Que vago oe, ya deja las pajas')
+});
